23f3137b8c99b9c8d095fc13590f0e953bb5dfe2
scrich <rich@scrich.co.uk>
2018-10-10 20:55:54 +0100

    Simple redirection detection - first commit

---
+++ Test Suites/TS Check redirections.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS Check redirections</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-10T20:52:11</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3a96420f-f287-41ff-99c2-051e2ea8101f</testSuiteGuid>
+   <testCaseLink>
+      <guid>9fd861e2-c3c9-467e-a471-1a5734bae6cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC verify redirected url</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>509e367f-2bb7-4e94-8de5-8132328d4183</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Redirection data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>509e367f-2bb7-4e94-8de5-8132328d4183</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>dRequestedUrl</value>
+         <variableId>27fda65b-e75a-4f01-b02d-62e4b9d46f6b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>509e367f-2bb7-4e94-8de5-8132328d4183</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>dExpectedUrl</value>
+         <variableId>3d1a53e6-8af0-4476-8494-2df5de0fe5f9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
